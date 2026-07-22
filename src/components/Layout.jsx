6777bc40cf65e558f2af01595d7a5af1f1db076a
Yashen Fernando <yashensavindu@gmail.com>
2026-07-22 20:47:48 +0530

    update: Scroll up in Redirected Page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Nav from './Nav';
 import Footer from './Footer';
@@ -5,6 +6,11 @@ import BackToTop from './BackToTop';
 
 export default function Layout() {
   const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div style={{ position: 'relative', minHeight: '100vh', background: 'var(--bg)', overflowX: 'hidden' }}>
       <Nav />
